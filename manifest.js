d6844197c7b1874a8ab47a7daa20e4682996c0f5
Jayus1010 <jayus.ali17@gmail.com>
2026-06-08 11:23:42 +0700

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,17 @@
+{
+  "short_name": "Hj. Rehana Memoriam", 
+  "name": "Hj. Rehana Memoriam", 
+  "icons": [
+    {
+      "src": "https://lh3.googleusercontent.com/u/0/d/1NM8uoz8tOqYxDbDDaqZWEAFOqv-NP0SQ", 
+      "type": "image/png",
+      "sizes": "512x512",
+      "purpose": "any maskable"
+    }
+  ],
+  "start_url": ".",
+  "background_color": "#ffffff", 
+  "theme_color": "#34a853", 
+  "display": "standalone",
+  "orientation": "portrait"
+}
